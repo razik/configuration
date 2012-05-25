4474ba96b66b7e54560bcd4c78697cf494b78907
Razik Yousfi <yousfi@systran.fr>
2012-05-25 11:33:08 +0200

    emacs: Update key bindings.

--- emacs/resources/basics/key-bindings.el
+++ emacs/resources/basics/key-bindings.el
@@ -7,16 +7,17 @@
 
 (global-set-key [f6]  'reindent-file)        ; f6 = reindent file
 (global-set-key [f7]  'goto-line)            ; f7 = goto line
-(global-set-key [f8]  'kill-this-buffer)     ; f8 = kill this buffer
+(global-set-key [f8]  'delete-window)        ; f8 = kill this buffer
 
 (global-set-key [f9]  'compile)              ; f9 = compile
 (global-set-key [f10] 'recompile)            ; f10 = recompile
 (global-set-key [f11] 'previous-error)       ; f11 = previous error
 (global-set-key [f12] 'next-error)           ; f12 = next error
 
-(global-set-key [M-right] 'yic-next-buffer)  ; meta-right = next buffer
-(global-set-key [M-left] 'yic-prev-buffer)   ; meta-left = previous buffer
 (global-set-key [(meta backspace)] 'backward-kill-word)
+(global-set-key [(meta tab)]       'yic-last-used-buffer)
+
+(windmove-default-keybindings 'meta)         ; move between windows with meta-keypad
 
 ;;;;;;;;;;;;;;;;;;
 ;; Key commands ;;
